d619cac8bf86e9a8813802a73285d55ca122364b
OHASHI Hideya <ohachige@gmail.com>
2014-03-31 17:57:58 +0900

    coding polling.rb

---
+++ polling.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/ruby
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'rubygems'
+require 'redis'
+require 'ripple_rpc'
+require 'digest/md5'
+
+@@config = YAML.load_file('config.yml')
+
+class Redis
+  def setm(k, o)
+    set(k, Marshal.dump(o))
+  end
+  def getm(k)
+    m = get(k)
+    m ? Marshal.load(m) : nil
+  end
+end
+
+@@redis = Redis.new
+
+def getrpc(coinname)
+  d = @@config['coins'][coinname]
+  uri = "http://#{d['user']}:#{d['password']}@#{d['host']}:#{d['port']}"
+  BitcoinRPC.new(uri)
+end
+
+def getripplerpc
+  d = @@config['ripple']
+  account_id = d['account_id']
+  master_seed = d['master_seed']
+  uri = "http://#{d['host']}:#{d['port']}"
+  RippleRPC.new(uri, account_id, master_seed)
+end
+
+rrpc = getripplerpc
+
+interval = 10
+limit = 200
+min = @@redis.get('polling:ledger') || -1
+p min # TODO
+max = -1
+loop do
+  params = {
+    'account' => rrpc.account_id,
+    'ledger_index_min' => min,
+    'ledger_index_max' => max,
+    'limit' => limit,
+  }
+  result = rrpc.account_tx(params)
+  unless result['status'] == 'success'
+    sleep interval
+    next
+  end
+  lmin = result['ledger_index_min']
+  lmax = result['ledger_index_max']
+  txs = result['transactions']
+  if txs.empty?
+    sleep interval
+    next
+  elsif txs.size >= limit
+    raise txs # TODO
+  end
+  min = lmax + 1
+p [lmin, lmax, txs.size]
+  txs.each do |txo|
+    tx = txo['tx']
+    type = tx['TransactionType']
+    next unless type == 'Payment'
+    ledger_index = tx['ledger_index']
+    amount = tx['Amount']
+    next unless amount === Array
+    ai = amount['issuer']
+p [ai, rrpc.account_id]
+    next unless rrpc.account_id == ai
+    ac = amount['currency']
+    av = amount['value']
+    coins = @@config['coins']
+    coinid, coin = coins.find{|k,v|v['symbol'] == ac}
+p [coinid, coin]
+    next unless coinid
+    dst = tx['Destination']
+    tag = tx['DestinationTag']
+p [ledger_index, :pay, ai, av, dst, tag]
+    @@redis.keys('id:*').each do |k|
+      v = @@redis.getm(k)
+      next unless v[:rippleaddr] == dst
+      tag2 = Digest::MD5.digest(k).unpack('V')[0] & 0x7fffffff
+p [:found, k, tag, tag2, ledger_index]
+      # TODO next unless tag == tag2
+p [:move]
+      moveto = k
+      rpc = getrpc(coinid)
+      rpc.move('iou', moveto, amount)
+    end
+  end
+  @@redis.set('polling:ledger', min)
+end
